dc9b6abd67530039feb4a3a96797781afed7af71
ZanoLeafVN <111110161+ZanoLeafVN@users.noreply.github.com>
2023-06-17 10:41:47 +0700

    LOADSTRING.lua

--- LOADSTRING.lua
+++ LOADSTRING.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZanoLeafVN/SB-SR/main/A00PKIDD%20Hub%20%7C%20By%20a00pkidd"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZanoLeafVN/SB-SR/main/Loader.lua"))()
